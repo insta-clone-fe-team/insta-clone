449f448b367fc7e594566377aca7e241ffb8182f
haaymonster <imhayleylee@gmail.com>
2024-01-02 13:30:40 +0900

    test svg

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./index.css";
+import instagramTextIcon from "../../assets/svg/instagram.svg";
+import test from "../../assets/svg/test.svg";
+import instagramLogo from "../../assets/svg/instagramLogo.svg";
 
 const SideBar = () => {
-  return <div className="sidebar-wrapper">sidebar</div>;
+  return (
+    <div className="sidebar-wrapper">
+      sidebar
+      <img src={instagramLogo} alt="instagram"></img>
+    </div>
+  );
 };
 
 export default SideBar;
