44f55b30e2e5fbcf58e20a411af71baa392a9019
Yes2eul <yeseul5611@gmail.com>
2024-01-03 17:23:48 +0900

    링크 새창으로 열기, 탭 갑의 보안 방지

--- src/pages/Login/components/LoginForm/index.jsx
+++ src/pages/Login/components/LoginForm/index.jsx
@@ -62,7 +62,11 @@ const LoginForm = () => {
           </form>
           <div className="bottom">
             <div className="facebook-login">
-              <a href="https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Dko_KR%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522ngygwzdwcfi936s18w4vs7j8ijqrm17st6mdunfxrew0qnox%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dd92a4dc9-c247-432a-b1f7-f835953e1046%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522ngygwzdwcfi936s18w4vs7j8ijqrm17st6mdunfxrew0qnox%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=ko_KR&pl_dbl=0">
+              <a
+                href="https://www.facebook.com/login.php?skip_api_login=1&api_key=124024574287414&kid_directed_site=0&app_id=124024574287414&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fdialog%2Foauth%3Fclient_id%3D124024574287414%26locale%3Dko_KR%26redirect_uri%3Dhttps%253A%252F%252Fwww.instagram.com%252Faccounts%252Fsignup%252F%26response_type%3Dcode%252Cgranted_scopes%26scope%3Demail%26state%3D%257B%2522fbLoginKey%2522%253A%2522ngygwzdwcfi936s18w4vs7j8ijqrm17st6mdunfxrew0qnox%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dd92a4dc9-c247-432a-b1f7-f835953e1046%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fwww.instagram.com%2Faccounts%2Fsignup%2F%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3D%257B%2522fbLoginKey%2522%253A%2522ngygwzdwcfi936s18w4vs7j8ijqrm17st6mdunfxrew0qnox%2522%252C%2522fbLoginReturnURL%2522%253A%2522%252Ffxcal%252Fdisclosure%252F%253Fnext%253D%25252F%2522%257D%23_%3D_&display=page&locale=ko_KR&pl_dbl=0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src="facebook.png"></img>
                 <button>Facebook으로 로그인</button>
               </a>
@@ -83,12 +87,20 @@ const LoginForm = () => {
           <div className="app-text">앱을 다운로드하세요.</div>
           <div className="app-download_img">
             <div className="app-store">
-              <a href="https://apps.apple.com/us/app/instagram/id389801252?vt=lo">
+              <a
+                href="https://apps.apple.com/us/app/instagram/id389801252?vt=lo"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src="appstore.png"></img>
               </a>
             </div>
             <div className="google-play">
-              <a href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DE488430E-8A12-4C09-9CA3-0FC6603A8844%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge&pli=1">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=ig_mid%3DE488430E-8A12-4C09-9CA3-0FC6603A8844%26utm_campaign%3DloginPage%26utm_content%3Dlo%26utm_source%3Dinstagramweb%26utm_medium%3Dbadge&pli=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src="googleplay.png"></img>
               </a>
             </div>
