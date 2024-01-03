b13e5b7a2104b1fd456cb9ce3a33f56a145bc718
haaymonster <imhayleylee@gmail.com>
2024-01-03 17:50:45 +0900

    사이드바 메뉴

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import AddIcon from "./components/icons/AddIcon";
+import ExploreIcon from "./components/icons/ExploreIcon";
+import HomeIcon from "./components/icons/HomeIcon";
+import InsIcon from "./components/icons/InsIcon";
+import InsTextIcon from "./components/icons/InsTextIcon";
+import MessengerIcon from "./components/icons/MessengerIcon";
+import MoreIcon from "./components/icons/MoreIcon";
+import NoticeIcon from "./components/icons/NoticeIcon";
+import ReelsIcon from "./components/icons/ReelsIcon";
+import SearchIcon from "./components/icons/SearchIcon";
+import ThreadsIcon from "./components/icons/ThreadsIcon";
+import defualtAvatar from "../../assets/defualtAvatar.jpg";
 import "./index.css";
-import instagramTextIcon from "../../assets/svg/instagram.svg";
-import test from "../../assets/svg/test.svg";
-import instagramLogo from "../../assets/svg/instagramLogo.svg";
+import Profile from "./components/icons/Profile";
 
 const SideBar = () => {
   return (
     <div className="sidebar-wrapper">
-      sidebar
-      <img src={instagramLogo} alt="instagram"></img>
+      {/* <InsIcon></InsIcon> */}
+      <InsTextIcon></InsTextIcon>
+      <div className="main-menu">
+        <HomeIcon style={{ fill: "currentColor" }}></HomeIcon>
+        <SearchIcon></SearchIcon>
+        <ExploreIcon></ExploreIcon>
+        <ReelsIcon></ReelsIcon>
+        <MessengerIcon></MessengerIcon>
+        <NoticeIcon></NoticeIcon>
+        <AddIcon></AddIcon>
+        <Profile></Profile>
+      </div>
+      <ThreadsIcon></ThreadsIcon>
+      <MoreIcon></MoreIcon>
     </div>
   );
 };
